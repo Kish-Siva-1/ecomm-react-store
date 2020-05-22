5c28521a191267115be7ea967a85ef5ac1283101
Kish-Siva-1 <rodes88@msn.com>
2020-05-22 20:13:42 +0200

    will add stripe payment method

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,13 @@
 import { createSelector } from "reselect";
 
+const COLLECITON_ID_MAP = {
+  hats: 1,
+  sneakers: 2, 
+  jackets: 3, 
+  womens: 4, 
+  mens: 5
+}
+
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -9,11 +17,12 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => Object.keys(collections).map(key => collections[key]) 
-)
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections[collectionUrlParam]
+  createSelector([selectCollections], (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECITON_ID_MAP[collectionUrlParam]
+    )
   );
